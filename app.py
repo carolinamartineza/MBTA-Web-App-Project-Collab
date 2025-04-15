87dba713b9288ab35540d14a712afcd8790d0b39
carolinamartineza <cmartinezaparicio1@babson.edu>
2025-04-15 11:48:03 -0400

    Updata part_1

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near
 
 app = Flask(__name__)
 
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+@app.route("/", methods=["GET", "POST"])
+def index():
+    result = None
+    if request.method == "POST":
+        place = request.form["place"]
+        data = find_stop_near(place)
+        if data:
+            station, accessible = data
+            result = {
+                "place": place,
+                "station": station,
+                "accessible": "Yes" if accessible else "No"
+            }
+        else:
+            result = {"error": "Could not find a nearby station."}
+        return render_template("index.html", result=result)
 
 if __name__ == "__main__":
     app.run(debug=True)
