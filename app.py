63d724ee15b45271ce30c653a718f796737c8e6a
paulinavasconezo <pvasconez1@babson.edu>
2025-04-15 15:48:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,59 @@
 from flask import Flask, render_template, request
 from mbta_helper import find_stop_near, MAPBOX_TOKEN
+import sqlite3
+from db import init_db
+init_db()
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-    result = None
-    if request.method == "POST":
-        place = request.form["place"]
-        data = find_stop_near(place)
-        if data:
-            station, accessible, lat, lng = data
-            result = {
-                "place": place,
-                "station": station,
-                "accessible": "Yes" if accessible else "No",
-                "latitude": lat,
-                "longitude": lng
-            }
-        else:
-            result = {"error": "Could not find a nearby station."}
-    return render_template("index.html", result=result, MAPBOX_TOKEN=MAPBOX_TOKEN)
+    return render_template("index.html")
+
+@app.route("/nearest_mbta", methods=["POST"])
+def nearest_mbta():
+    place = request.form.get("place")
+
+    if not place:
+        return render_template("error.html", message="No location provided.")
+
+    result = find_stop_near(place)
+    if result:
+        station, accessible, lat, lng = result
+
+        conn = sqlite3.connect("mbta.db")
+        cur = conn.cursor()
+
+        cur.execute("""
+            INSERT INTO searches (place, station, accessible, latitude, longitude)
+            VALUES (?, ?, ?, ?, ?)
+        """, (place, station, accessible, lat, lng))
+
+        conn.commit()
+        conn.close()
+
+        return render_template("mbta_station.html",
+                                place=place,
+                                station=station,
+                                accessible="Yes" if accessible else "No",
+                                latitude=lat,
+                                longitude=lng,
+                                MAPBOX_TOKEN=MAPBOX_TOKEN)
+    else:
+        return render_template("error.html", message="Could not find a nerby stop.")
+
+@app.route("/history")
+def history():
+    conn = sqlite3.connect("mbta.db")
+    cur = conn.cursor()
+
+    cur.execute("SELECT place, station, accessible, latitude, longitude FROM searches ORDER BY id DESC")
+    rows = cur.fetchall()
+
+    conn.close()
+
+    return render_template("history,html", searches=rows)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
